993f3fbfc4dc486bb38e8c21f71ba545b72e7a12
Luan Costa <luan.coast@outlook.com.br>
2022-11-07 21:04:07 -0300

    Separação por grupo e correção da responsividade

--- main.js
+++ main.js
@@ -28,32 +28,39 @@ function createCard(date, day, games) {
 
 }
 
+let delay2 = -0.3;
 
-document.querySelector("#cards").innerHTML = 
+function createGroup(group, player1, player2, player3, player4) {
+    
+    delay2 = delay2 + 0.3;
 
-    
-        createCard(
-            '24/11', 
-            'quinta',
-            createGame('switzerland', '07:00', 'cameroon')+
-            createGame('uruguay', '10:00', 'south-korea')+
-            createGame('portugal', '13:00', 'ghana')+
-            createGame('brazil', '16:00', 'serbia'))
-    
-        + createCard(
-            '28/11', 
-            'segunda', 
-            createGame('cameroon', '07:00', 'serbia')+
-            createGame('south-korea', '10:00', 'ghana')+
-            createGame('brazil', '13:00', 'switzerland')+
-            createGame('portugal', '13:00', 'uruguay'))
-        
-        + createCard(
-            '02/12', 
-            'sexta', 
-            createGame('south-korea', '12:00', 'portugal')+
-            createGame('ghana', '12:00', 'uruguay')+
-            createGame('serbia', '16:00', 'switzerland')+
-            createGame('cameroon', '16:00', 'brazil'))
+    return `
+    <div class="index" style="animation-delay: ${delay2}s">
+                    
+                    <h2>Grupo ${group}</h2>
+                    <ul>
+                        <li>
+                            <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+                            <span>${player1}</span>
+                        </li>
+                        <li><img src="./assets/icon-${player2}.svg" alt="Bandeira do ${player2}">
+                        <span>${player2}</span>
+                        </li>
+                        <li>
+                            <img src="./assets/icon-${player3}.svg" alt="Bandeira do ${player3}">
+                            <span>${player3}</span>
+                        </li>
+                        <li>
+                            <img src="./assets/icon-${player4}.svg" alt="Bandeira do ${player4}">
+                            <span>${player4}</span>
+                        </li>
+                    </ul>
+                    
+                </div>
+    `
+}
+
+
+
         
 
